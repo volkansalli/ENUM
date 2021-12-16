1817ef09652a556fc7a71af861a0d60b33206579
volkan_salli <volkan_salli@hotmail.com>
2021-12-16 03:19:58 +0300

    Add files via upload

---
+++ enum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(gunler.Sali);
+            Console.WriteLine((int)gunler.Sali);
+            int hava=22;
+            if(hava>=(int)sicaklik.normal && hava<=(int)sicaklik.coksicak)
+            Console.WriteLine("hadi disariya cikalim");
+        }
+    }
+}
+enum gunler
+{
+Pazartesi=1,
+Sali,
+Çarşamba,
+Perşembe,
+Cuma,
+Cumartesi,
+Pazar
+}
+enum sicaklik
+{
+    soguk=15,
+    normal =22,
+    sicak=30,
+    coksicak=35
+
+}
